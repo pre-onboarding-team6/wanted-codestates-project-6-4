f6393595e8051af6732d5561a2cb9dcec62ec0c0
hyejeong <withoo0414@gmail.com>
2022-03-14 15:48:03 +0900

    Design: global style적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Global } from '@emotion/react';
+import { reset } from './styles/reset';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <Global styles={reset} />
+      <div className="App">App</div>
+    </>
+  );
 }
 
 export default App;
