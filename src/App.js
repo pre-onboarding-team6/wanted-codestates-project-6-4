a442369d4a24f85b88c0b6b6ab8a8583b2026d51
hyejeong <withoo0414@gmail.com>
2022-03-15 00:19:37 +0900

    Comment: likeshare component 주석추가

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ import 'html5-device-mockups/dist/device-mockups.min.css';
 import { Global } from '@emotion/react';
 import { reset } from './styles/reset';
 import styled from '@emotion/styled';
-import { ContentBox } from './styles/styles';
-import Thumbnail from './components/Thumbnail';
-import { backgroundColor, fontSize, iconSize } from './styles/colors';
-import Subscription from './components/Subscribe';
-import LikeShare from './components/LikeShare';
+import { backgroundColor } from './styles/colors';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from './redux/reducers/infoDataReducer';
 import SlidingPage from './components/SlidingPage';
+
 import Tabs from './components/Tabs';
 import loader from './assets/loader.png';
 
 function App() {
   const dispatch = useDispatch();
-  const [likeCount, setLikeCount] = useState(0);
   const { data, loading } = useSelector((state) => state.infoData);
   const [tab, setTab] = useState(0);
 
@@ -61,24 +58,6 @@ function App() {
                 />
               </>
             )}
-            <Subscription />
-            {/* 새로 올라왔어요에서 like, share 사이즈는 기본으로 사용 */}
-            <LikeShare />
-            <LikeShare
-              likeText={'좋아요'}
-              shareText={'공유하기'}
-              fontSize={fontSize.base} // 디테일 페이지 사이즈
-              iconSize={iconSize.base}
-              link="https://sandbank.io" // 컨텐츠 링크
-            />
-            <LikeShare
-              likeText={likeCount}
-              setLikeCount={setLikeCount}
-              shareText={'공유하기'}
-              fontSize={fontSize.sm} // 더보기 컴포넌트 사이즈
-              iconSize={iconSize.sm}
-              link="https://sandbank.io"
-            />
           </ViewPort>
         </IPhoneSE>
       </AppContainer>
