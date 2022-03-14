0829ec7089e7411245ed59210a4aca3ccf119357
madstone-dev <hanbin8570@gmail.com>
2022-03-14 16:35:59 +0900

    Design : 콘텐츠 박스 & 이미지 썸네일

---
+++ src/styles/styles.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const ContentBox = styled.div`
+  padding: 1rem;
+  border-radius: 1rem;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  background-color: white;
+`;
